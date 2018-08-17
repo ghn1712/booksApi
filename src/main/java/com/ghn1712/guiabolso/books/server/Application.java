b70095aaf16d3c1fe855c891133ca77e5f092edb
Guilherme Hernandes <guilherme.nascimento@grupopan.com>
2018-08-17 17:51:45 -0300

    Create routes on server

--- src/main/java/com/ghn1712/guiabolso/books/server/Application.java
+++ src/main/java/com/ghn1712/guiabolso/books/server/Application.java
@@ -1,5 +1,25 @@
 package com.ghn1712.guiabolso.books.server;
 
+import static spark.Spark.get;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.ghn1712.guiabolso.books.controllers.BooksController;
+import com.ghn1712.guiabolso.books.injection.modules.BooksModule;
+import com.ghn1712.guiabolso.books.serializer.Serializer;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 public class Application {
+
+    public static void main(String[] args) {
+        Injector injector = Guice.createInjector(new BooksModule());
+        BooksController controller = injector.getInstance(BooksController.class);
+        get("/books", (req, res) -> Serializer.serialize(controller.listBooks()));
+        get("/books/:id",
+                (req, res) -> controller.getBookById(req.params("id")).map(Serializer::serialize).orElseGet(() -> {
+                    res.status(404);
+                    return StringUtils.EMPTY;
+                }));
+    }
 }
