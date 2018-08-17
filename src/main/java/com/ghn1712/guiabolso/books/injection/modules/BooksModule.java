95d34ee429772b27120faddc885be3d5bac496b8
Guilherme Hernandes <ghn1712@gmail.com>
2018-08-17 00:45:51 -0300

    Add injection module class

---
+++ src/main/java/com/ghn1712/guiabolso/books/injection/modules/BooksModule.java
@@ -0,0 +1,10 @@
+package com.ghn1712.guiabolso.books.injection.modules;
+import com.google.inject.AbstractModule;
+
+public class BooksModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        super.configure();
+    }
+}
