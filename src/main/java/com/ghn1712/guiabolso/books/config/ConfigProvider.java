b5474d2506d399c2f43f98181f2a55265a861cdd
Guilherme Hernandes <guilherme.nascimento@grupopan.com>
2018-08-17 11:38:51 -0300

    Add config provider interface do decouple

---
+++ src/main/java/com/ghn1712/guiabolso/books/config/ConfigProvider.java
@@ -0,0 +1,6 @@
+package com.ghn1712.guiabolso.books.config;
+
+public interface ConfigProvider {
+
+    Config getConfig();
+}
--- src/main/java/com/ghn1712/guiabolso/books/config/ConfigProviderImpl.java
+++ src/main/java/com/ghn1712/guiabolso/books/config/ConfigProviderImpl.java
@@ -1,13 +1,12 @@
 package com.ghn1712.guiabolso.books.config;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
 import com.google.gson.Gson;
 
-public class ConfigProviderImpl {
+public class ConfigProviderImpl implements ConfigProvider {
 
     private static final String CONFIG_FILE_NAME = "config/config.json";
     private final Config config;
@@ -21,6 +20,7 @@ public class ConfigProviderImpl {
         return new Gson().fromJson(new String(Files.readAllBytes(file.toPath())), Config.class);
     }
 
+    @Override
     public Config getConfig() {
         return config;
     }
