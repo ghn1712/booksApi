c12487a5126b95717ed9670372a8658a4e667759
Guilherme Hernandes <guilherme.nascimento@grupopan.com>
2018-08-17 17:52:40 -0300

    Rebase previous ca2c3ef

---
+++ src/main/java/com/ghn1712/guiabolso/books/serializer/Serializer.java
@@ -0,0 +1,24 @@
+package com.ghn1712.guiabolso.books.serializer;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+public class Serializer {
+
+    private Serializer() {
+    }
+
+    private static final Gson gson = new Gson();
+
+    public static <T> T deserialize(String json) {
+        return gson.fromJson(json, new TypeToken<T>() {}.getType());
+    }
+
+    public static <T> T deserialize(String json, Class<T> clazz) {
+        return gson.fromJson(json, clazz);
+    }
+
+    public static <T> String serialize(T object) {
+        return gson.toJson(object, new TypeToken<T>(object.getClass()) {}.getType());
+    }
+}
