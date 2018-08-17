9fb30c9fd56ff6701fc4f11d44c1d10647a8582a
Guilherme Hernandes <guilherme.nascimento@grupopan.com>
2018-08-17 11:40:00 -0300

    Fix controller to implement properly interface

--- src/main/java/com/ghn1712/guiabolso/books/controllers/BooksControllerImpl.java
+++ src/main/java/com/ghn1712/guiabolso/books/controllers/BooksControllerImpl.java
@@ -7,7 +7,7 @@ import javax.inject.Inject;
 import com.ghn1712.guiabolso.books.entities.Book;
 import com.ghn1712.guiabolso.books.usecases.BooksUsecase;
 
-public class BooksControllerImpl {
+public class BooksControllerImpl implements BooksController {
 
     BooksUsecase booksUsecase;
 
@@ -16,16 +16,18 @@ public class BooksControllerImpl {
         this.booksUsecase = booksUsecase;
     }
 
+    @Override
     public List<Book> listBooks() {
         return booksUsecase.listBooks();
     }
 
+    @Override
     public Book getBook(String id) {
         return booksUsecase.getBook(id);
     }
 
+    @Override
     public String addBook(Book book) {
         return booksUsecase.addBook(book);
     }
-
 }
