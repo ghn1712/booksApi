c75ae77e34976b90459a0891105a9576fc0871ce
Guilherme Hernandes <ghn1712@gmail.com>
2018-08-18 14:41:32 -0300

    Fix isbn retrieve not found message

--- src/main/java/com/ghn1712/guiabolso/books/crawler/IsbnStrategy.java
+++ src/main/java/com/ghn1712/guiabolso/books/crawler/IsbnStrategy.java
@@ -3,6 +3,6 @@ package com.ghn1712.guiabolso.books.crawler;
 public interface IsbnStrategy {
 
     default String execute(String url) {
-        return "undefined";
+        return "Unavailable";
     }
 }
