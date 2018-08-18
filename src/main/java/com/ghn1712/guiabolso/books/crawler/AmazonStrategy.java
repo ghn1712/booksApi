79227c1f277bdb4b1aa0f31e999c048878afb844
Guilherme Hernandes <ghn1712@gmail.com>
2018-08-17 23:02:57 -0300

    Add amazon books isbn retrieve implementation

--- src/main/java/com/ghn1712/guiabolso/books/crawler/AmazonStrategy.java
+++ src/main/java/com/ghn1712/guiabolso/books/crawler/AmazonStrategy.java
@@ -1,9 +1,20 @@
 package com.ghn1712.guiabolso.books.crawler;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+
 public class AmazonStrategy implements IsbnStrategy {
 
     @Override
     public String execute(String url) {
-        return null;
+        try {
+            return Jsoup.connect(url).get().select("div.content li").eachText().parallelStream()
+                    .filter(text -> text.split(":")[0].equals("ISBN-13")).findAny().orElse("undefined");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "undefined";
     }
 }
