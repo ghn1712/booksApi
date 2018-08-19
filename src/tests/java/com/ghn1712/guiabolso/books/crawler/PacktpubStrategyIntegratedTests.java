93cf80530ee5ed6475714ca6b44b542d18e5be64
Guilherme Hernandes <ghn1712@gmail.com>
2018-08-19 12:12:52 -0300

    Add packtpub strategy tests

---
+++ src/tests/java/com/ghn1712/guiabolso/books/crawler/PacktpubStrategyIntegratedTests.java
@@ -0,0 +1,41 @@
+package com.ghn1712.guiabolso.books.crawler;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.UncheckedIOException;
+import java.util.regex.Pattern;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PacktpubStrategyIntegratedTests {
+
+    static PacktpubStrategy strategy;
+    static Pattern pattern;
+    static String regex = "^[0-9]{13}$";
+
+    @BeforeClass
+    public static void set_up() {
+        strategy = new PacktpubStrategy();
+        pattern = Pattern.compile(regex);
+    }
+
+    @Test
+    public void should_return_book_isbn_when_connecting_to_packtub_website() {
+        String isbn = strategy.execute("https://www.packtpub.com/application-development/net-design-patterns");
+        assertTrue(pattern.matcher(isbn).matches());
+        assertEquals("9781786466150", isbn);
+    }
+
+    @Test
+    public void should_return_unavailable_when_connecting_to_google_website() {
+        String isbn = strategy.execute("https://www.google.com");
+        assertEquals("Unavailable", isbn);
+    }
+
+    @Test(expected = UncheckedIOException.class)
+    public void should_throw_exception_when_cant_connect_to_website() {
+        strategy.execute("http://localhost:4568");
+    }
+}
