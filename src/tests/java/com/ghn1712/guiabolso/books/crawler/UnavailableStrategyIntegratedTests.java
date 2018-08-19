9aeeec93d4ea796346587ae8f68f2ef262f6df89
Guilherme Hernandes <ghn1712@gmail.com>
2018-08-19 12:13:05 -0300

    Add unavailable strategy tests

---
+++ src/tests/java/com/ghn1712/guiabolso/books/crawler/UnavailableStrategyIntegratedTests.java
@@ -0,0 +1,22 @@
+package com.ghn1712.guiabolso.books.crawler;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnavailableStrategyIntegratedTests {
+
+    UnavailableStrategy strategy;
+
+    @Before
+    public void set_up() {
+        strategy = new UnavailableStrategy();
+    }
+
+    @Test
+    public void should_return_unavailable() {
+        assertEquals("Unavailable", strategy.execute(
+                "https://docs.gradle.org/current/userguide/userguide_single.html?_ga=2.136842420.542867609.1534655620-1469846802.1526948723#java_testing"));
+    }
+}
