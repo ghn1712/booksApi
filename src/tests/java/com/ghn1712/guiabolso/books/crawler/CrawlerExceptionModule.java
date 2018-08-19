aa282b4f7e199edf0b67fb9a9ba35a6fe201164b
Guilherme Hernandes <ghn1712@gmail.com>
2018-08-19 03:03:21 -0300

    Add crawler tests

---
+++ src/tests/java/com/ghn1712/guiabolso/books/crawler/CrawlerExceptionModule.java
@@ -0,0 +1,22 @@
+package com.ghn1712.guiabolso.books.crawler;
+
+import com.ghn1712.guiabolso.books.config.CrawlerConfig;
+import com.ghn1712.guiabolso.books.gateways.BooksCrawlerGateway;
+import com.ghn1712.guiabolso.books.gateways.BooksListGateway;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+
+public class CrawlerExceptionModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(BooksListGateway.class).to(BooksCrawlerGateway.class);
+    }
+
+    @Provides
+    public CrawlerConfig provideCrawlerConfig() {
+        CrawlerConfig crawlerConfig = new CrawlerConfig();
+        crawlerConfig.setUrl("http://localhost:4568");
+        return crawlerConfig;
+    }
+}
---
+++ src/tests/java/com/ghn1712/guiabolso/books/crawler/CrawlerIntegratedTests.java
@@ -0,0 +1,42 @@
+package com.ghn1712.guiabolso.books.crawler;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.UncheckedIOException;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.ghn1712.guiabolso.books.entities.Book;
+import com.ghn1712.guiabolso.books.gateways.BooksListGateway;
+import com.ghn1712.guiabolso.books.injection.modules.BooksModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class CrawlerIntegratedTests {
+
+    BooksListGateway crawler;
+
+    @Test
+    public void should_return_kotlin_language_website_list() {
+        Injector injector = Guice.createInjector(new BooksModule());
+        crawler = injector.getInstance(BooksListGateway.class);
+        List<Book> response = crawler.listBooks();
+        assertEquals(24, response.size());
+    }
+
+    @Test
+    public void should_return_empty_list() {
+        Injector injector = Guice.createInjector(new CrawlerModule());
+        crawler = injector.getInstance(BooksListGateway.class);
+        List<Book> response = crawler.listBooks();
+        assertEquals(0, response.size());
+    }
+
+    @Test(expected = UncheckedIOException.class)
+    public void should_throw_exception_when_cant_connect_to_url() {
+        Injector injector = Guice.createInjector(new CrawlerExceptionModule());
+        crawler = injector.getInstance(BooksListGateway.class);
+        crawler.listBooks();
+    }
+}
