b7d878b5d9687eae1bdccaad91f89fd61903be70
Guilherme Hernandes <ghn1712@gmail.com>
2018-08-19 12:11:38 -0300

    Add isbn retriever context tests

---
+++ src/tests/java/com/ghn1712/guiabolso/books/crawler/IsbnRetrieverContextIntegratedTest.java
@@ -0,0 +1,31 @@
+package com.ghn1712.guiabolso.books.crawler;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.regex.Pattern;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class IsbnRetrieverContextIntegratedTest {
+
+    static AmazonStrategy strategy;
+    static Pattern pattern;
+    static String regex = "^[0-9]{13}$";
+
+    @BeforeClass
+    public static void set_up() {
+        strategy = new AmazonStrategy();
+        pattern = Pattern.compile(regex);
+    }
+
+    @Test
+    public void should_return_book_isbn_when_connecting_to_amazon_website() {
+        String isbn = IsbnRetrieverContext.getIsbn(
+                "https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882/ref=sr_1_1_sspa?ie=UTF8&qid=1534625054&sr=8-1-spons&keywords=clean+code&psc=1",
+                strategy);
+        assertTrue(pattern.matcher(isbn).matches());
+        assertEquals("9780132350884", isbn);
+    }
+}
