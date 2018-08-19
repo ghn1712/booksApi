99a4606b77052fb05e9d20a7a008df21800a7ddc
Guilherme Hernandes <ghn1712@gmail.com>
2018-08-19 12:11:14 -0300

    Add fundamental kotlin strategy tests

--- src/main/java/com/ghn1712/guiabolso/books/crawler/FundamentalKotlinStrategy.java
+++ src/main/java/com/ghn1712/guiabolso/books/crawler/FundamentalKotlinStrategy.java
@@ -1,6 +1,7 @@
 package com.ghn1712.guiabolso.books.crawler;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 
 import org.jsoup.Jsoup;
 
@@ -8,15 +9,13 @@ public class FundamentalKotlinStrategy implements IsbnRetrieverStrategy {
 
     @Override
     public String execute(String url) {
-        final String defaultResponse = IsbnRetrieverStrategy.super.execute(url);
         try {
             return Jsoup.connect(url).get().select("div.scondary_content h2.dark-blue-text").eachText().parallelStream()
-                    .filter(text -> text.split(":")[0].equals("ISBN")).map(text -> text.split(":")[1]).findAny()
-                    .orElse(defaultResponse);
+                    .filter(text -> text.split(" ")[0].equals("ISBN:")).map(text -> text.split(" ")[1]).findAny()
+                    .orElse(IsbnRetrieverStrategy.super.execute(url));
         }
         catch (IOException e) {
-            e.printStackTrace();
+            throw new UncheckedIOException(e);
         }
-        return defaultResponse;
     }
 }
---
+++ src/tests/java/com/ghn1712/guiabolso/books/crawler/FundamentalKotlinStrategyIntegratedTests.java
@@ -0,0 +1,41 @@
+package com.ghn1712.guiabolso.books.crawler;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.UncheckedIOException;
+import java.util.regex.Pattern;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FundamentalKotlinStrategyIntegratedTests {
+
+    static FundamentalKotlinStrategy strategy;
+    static Pattern pattern;
+    static String regex = "^[0-9]{13}$";
+
+    @BeforeClass
+    public static void set_up() {
+        strategy = new FundamentalKotlinStrategy();
+        pattern = Pattern.compile(regex);
+    }
+
+    @Test
+    public void should_return_book_isbn_when_connecting_to_fundamental_kotlin_website() {
+        String isbn = strategy.execute("http://www.fundamental-kotlin.com/");
+        assertTrue(pattern.matcher(isbn).matches());
+        assertEquals("9788692030710", isbn);
+    }
+
+    @Test
+    public void should_return_unavailable_when_connecting_to_google_website() {
+        String isbn = strategy.execute("https://www.google.com");
+        assertEquals("Unavailable", isbn);
+    }
+
+    @Test(expected = UncheckedIOException.class)
+    public void should_throw_exception_when_cant_connect_to_website() {
+        strategy.execute("http://localhost:4568");
+    }
+}
